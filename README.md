2f63bd115ca41ad0981bff6775bd7c41cc3cf182
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2023-09-04 20:55:36 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CCMDesignSystem
+
+A description of this package.
---
+++ Sources/CCMDesignSystem/CCMDesignSystem.swift
@@ -0,0 +1,6 @@
+public struct CCMDesignSystem {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
